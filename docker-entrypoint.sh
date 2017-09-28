c9c991719213ba463e9903d955aa1cf26b0544b8
Christoph Kappestein <christoph.kappestein@gmail.com>
2017-09-28 22:34:48 +0200

    run deploy on startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,31 +5,38 @@ while ! mysqladmin ping -h"$FUSIO_DB_HOST" --silent; do
     sleep 1
 done
 
+pushd /var/www/html/fusio
+
 # install fusio
-/usr/bin/php /var/www/html/fusio/bin/fusio system:check install
+php bin/fusio system:check install
 exitCode=$?
 if [ $exitCode -ne 0 ]; then
-    /usr/bin/php /var/www/html/fusio/bin/fusio install
+    php bin/fusio install
 
     # adjust js apps url
-    find /var/www/html/fusio/public/ -type f -exec sed -i 's#\${FUSIO_URL}#'"$FUSIO_URL"'#g' {} \;
+    find public/ -type f -exec sed -i 's#\${FUSIO_URL}#'"$FUSIO_URL"'#g' {} \;
 
     # register adapters
-    /usr/bin/php /var/www/html/fusio/bin/fusio system:register -y "Fusio\Adapter\Amqp\Adapter"
-    /usr/bin/php /var/www/html/fusio/bin/fusio system:register -y "Fusio\Adapter\Beanstalk\Adapter"
-    /usr/bin/php /var/www/html/fusio/bin/fusio system:register -y "Fusio\Adapter\Elasticsearch\Adapter"
-    /usr/bin/php /var/www/html/fusio/bin/fusio system:register -y "Fusio\Adapter\Memcache\Adapter"
-    /usr/bin/php /var/www/html/fusio/bin/fusio system:register -y "Fusio\Adapter\Mongodb\Adapter"
-    /usr/bin/php /var/www/html/fusio/bin/fusio system:register -y "Fusio\Adapter\Redis\Adapter"
-    /usr/bin/php /var/www/html/fusio/bin/fusio system:register -y "Fusio\Adapter\Soap\Adapter"
+    php bin/fusio system:register -y "Fusio\Adapter\Amqp\Adapter"
+    php bin/fusio system:register -y "Fusio\Adapter\Beanstalk\Adapter"
+    php bin/fusio system:register -y "Fusio\Adapter\Elasticsearch\Adapter"
+    php bin/fusio system:register -y "Fusio\Adapter\Memcache\Adapter"
+    php bin/fusio system:register -y "Fusio\Adapter\Mongodb\Adapter"
+    php bin/fusio system:register -y "Fusio\Adapter\Redis\Adapter"
+    php bin/fusio system:register -y "Fusio\Adapter\Soap\Adapter"
 fi
 
 # add initial backend user
-/usr/bin/php /var/www/html/fusio/bin/fusio system:check user
+php bin/fusio system:check user
 exitCode=$?
 if [ $exitCode -ne 0 ]; then
-    /usr/bin/php /var/www/html/fusio/bin/fusio user:add --status=1 --username="$FUSIO_BACKEND_USER" --email="$FUSIO_BACKEND_EMAIL" --password="$FUSIO_BACKEND_PW"
+    php bin/fusio user:add --status=1 --username="$FUSIO_BACKEND_USER" --email="$FUSIO_BACKEND_EMAIL" --password="$FUSIO_BACKEND_PW"
 fi
+
+# deploy
+php bin/fusio deploy
+
+popd
 
 # start apache
 source /etc/apache2/envvars
