ee125ea48a026a66b1d1f87302e6426c07ea912b
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-02-09 21:26:15 +0100

    update version

--- tests/Api/WelcomeTest.php
+++ tests/Api/WelcomeTest.php
@@ -23,7 +23,7 @@ class WelcomeTest extends HttpTestCase
         $expect = <<<JSON
 {
     "message": "Congratulations the installation of Fusio was successful",
-    "apiVersion": "v4.3.3@[hash]",
+    "apiVersion": "v4.3.7@[hash]",
     "links": [
         {
             "rel": "about",
