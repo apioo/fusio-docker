731eca5d6085275d859036e29bd60f060f08e40b
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-12-31 02:03:58 +0100

    use role instead of status option

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -27,7 +27,7 @@ fi
 php bin/fusio system:check user
 exitCode=$?
 if [ $exitCode -ne 0 ]; then
-    php bin/fusio user:add --status=1 --username="$FUSIO_BACKEND_USER" --email="$FUSIO_BACKEND_EMAIL" --password="$FUSIO_BACKEND_PW"
+    php bin/fusio user:add --role=1 --username="$FUSIO_BACKEND_USER" --email="$FUSIO_BACKEND_EMAIL" --password="$FUSIO_BACKEND_PW"
 
     # register adapters
     php bin/fusio system:register -y "Fusio\Adapter\Amqp\Adapter"
