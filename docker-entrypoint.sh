b0d9883129e04e775900d64b929fb7a6aa804741
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-12-31 02:03:38 +0100

    register initial adapters and install backend app only once

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,16 +13,6 @@ exitCode=$?
 if [ $exitCode -ne 0 ]; then
     # migrate fusio
     php bin/fusio migration:migrate --no-interaction
-
-    # register adapters
-    php bin/fusio system:register -y "Fusio\Adapter\Amqp\Adapter"
-    php bin/fusio system:register -y "Fusio\Adapter\Beanstalk\Adapter"
-    php bin/fusio system:register -y "Fusio\Adapter\Elasticsearch\Adapter"
-    php bin/fusio system:register -y "Fusio\Adapter\Memcache\Adapter"
-    php bin/fusio system:register -y "Fusio\Adapter\Mongodb\Adapter"
-    php bin/fusio system:register -y "Fusio\Adapter\Redis\Adapter"
-    php bin/fusio system:register -y "Fusio\Adapter\Smtp\Adapter"
-    php bin/fusio system:register -y "Fusio\Adapter\Soap\Adapter"
 fi
 
 # install app
@@ -38,13 +28,23 @@ php bin/fusio system:check user
 exitCode=$?
 if [ $exitCode -ne 0 ]; then
     php bin/fusio user:add --status=1 --username="$FUSIO_BACKEND_USER" --email="$FUSIO_BACKEND_EMAIL" --password="$FUSIO_BACKEND_PW"
+
+    # register adapters
+    php bin/fusio system:register -y "Fusio\Adapter\Amqp\Adapter"
+    php bin/fusio system:register -y "Fusio\Adapter\Beanstalk\Adapter"
+    php bin/fusio system:register -y "Fusio\Adapter\Elasticsearch\Adapter"
+    php bin/fusio system:register -y "Fusio\Adapter\Memcache\Adapter"
+    php bin/fusio system:register -y "Fusio\Adapter\Mongodb\Adapter"
+    php bin/fusio system:register -y "Fusio\Adapter\Redis\Adapter"
+    php bin/fusio system:register -y "Fusio\Adapter\Smtp\Adapter"
+    php bin/fusio system:register -y "Fusio\Adapter\Soap\Adapter"
+
+    # install backend app
+    php bin/fusio marketplace:install fusio
 fi
 
 # deploy
 php bin/fusio deploy
-
-# install backend app
-php bin/fusio marketplace:install fusio
 
 popd
 
