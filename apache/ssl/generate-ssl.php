5145e42392802593aa08d45af3a5fd64329cc971
Christoph Kappestein <christoph.kappestein@gmail.com>
2021-10-03 16:10:31 +0200

    directly use certbot

--- apache/ssl/generate-ssl.php
+++ apache/ssl/generate-ssl.php
@@ -6,39 +6,6 @@ if (empty($domain)) {
     exit(1);
 }
 
-$host = getenv('FUSIO_HOST');
-if (empty($host)) {
-    file_put_contents('/home/ssl.log', 'Environment variable FUSIO_HOST not available');
-    exit(1);
-}
-
-// resolve ip for domain
-$ip = resolveIp($domain);
-if (empty($ip)) {
-    file_put_contents('/home/ssl.log', 'Could not resolve IP for ' . $domain);
-    exit(1);
-}
-
-$response = file_get_contents('http://' . $host . '/system/health');
-if (empty($response)) {
-    file_put_contents('/home/ssl.log', 'The host ' . $host . ' is not reachable');
-    exit(1);
-}
-
-// check that the response is valid
-$data = \json_decode($response);
-if (!$data instanceof stdClass) {
-    file_put_contents('/home/ssl.log', 'The host ' . $host . ' returned an invalid response');
-    exit(1);
-}
-
-// check that the system is healthy
-$healthy = $data->healthy ?? false;
-if ($healthy !== true) {
-    file_put_contents('/home/ssl.log', 'The host ' . $host . ' is unhealthy');
-    exit(1);
-}
-
 // register cert for domain
 exec('certbot --apache -d ' . $domain, $output, $exitCode);
 if ($exitCode !== 0) {
@@ -48,18 +15,3 @@ if ($exitCode !== 0) {
 
 // remove ssl-cron on success
 unlink('/etc/cron.d/ssl');
-
-
-function resolveIp(string $domain): ?string
-{
-    $records = dns_get_record($domain, DNS_A | DNS_AAAA);
-    foreach ($records as $row) {
-        if ($row['type'] === 'A') {
-            return $row['ip'];
-        } elseif ($row['type'] === 'AAAA') {
-            return $row['ip'];
-        }
-    }
-
-    return null;
-}
