1cd8be8645b01a6385b937b52e60d34836f86109
Christoph Kappestein <christoph.kappestein@gmail.com>
2021-10-05 12:16:17 +0200

    add env to indicate whether to automatically obtain a cert

--- apache/generate-ssl.php
+++ apache/generate-ssl.php
@@ -5,15 +5,22 @@ const WAIT = 30;
 
 $domain = getenv('FUSIO_DOMAIN');
 $email = getenv('FUSIO_BACKEND_EMAIL');
+$cerbot = getenv('FUSIO_CERTBOT');
+
+if (empty($cerbot)) {
+    // in this case we dont want to obtain a cert automatically
+    unlink(__FILE__);
+    exit(0);
+}
 
 echo 'Try to obtain SSL cert for ' . $domain . "\n";
 
 $count = 1;
 $success = false;
-while ($count <= MAX_TRY) {
+while (!empty($domain) && $count <= MAX_TRY) {
     sleep(WAIT * $count);
 
-    exec('certbot --apache -d ' . $domain . ' --agree-tos -m ' . $email, $output, $exitCode);
+    exec('certbot --apache -d ' . escapeshellarg($domain) . ' --agree-tos -m ' . escapeshellarg($email), $output, $exitCode);
     if ($exitCode === 0) {
         echo 'Obtained SSL cert for ' . $domain . "\n";
         break;
@@ -26,4 +33,7 @@ while ($count <= MAX_TRY) {
 }
 
 // remove file
+echo 'Remove obtain SSL cert script' . "\n";
+
 unlink(__FILE__);
+exit(0);
