adfb25df523e4341dddacedee6505a4359907a2a
Christoph Kappestein <christoph.kappestein@gmail.com>
2021-12-21 20:21:21 +0100

    fix test case

--- tests/Api/WelcomeTest.php
+++ tests/Api/WelcomeTest.php
@@ -22,7 +22,7 @@ class WelcomeTest extends HttpTestCase
 
         $expect = <<<JSON
 {
-  "apiVersion": "5.1.8.0",
+  "apiVersion": "5.2.1.0",
   "title": "Fusio",
   "categories": [
     "authorization",
