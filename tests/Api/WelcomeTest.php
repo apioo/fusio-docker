32bd42ee8dc6e45f63b7d5648c6ef623ee619ead
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-03-17 22:42:58 +0100

    update version

--- tests/Api/WelcomeTest.php
+++ tests/Api/WelcomeTest.php
@@ -22,7 +22,7 @@ class WelcomeTest extends HttpTestCase
 
         $expect = <<<JSON
 {
-  "apiVersion": "7.2.5.0",
+  "apiVersion": "7.2.6.0",
   "title": "Fusio",
   "paymentCurrency": "EUR",
   "categories": [
