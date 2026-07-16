b6d159d9cca903c454c1ed15bac3533eb9d0d5d4
Christoph Kappestein <christoph.kappestein@gmail.com>
2026-07-16 22:26:54 +0200

    fix test

--- tests/Api/WelcomeTest.php
+++ tests/Api/WelcomeTest.php
@@ -22,7 +22,7 @@ class WelcomeTest extends HttpTestCase
 
         $expect = <<<JSON
 {
-  "apiVersion": "v8.8.3",
+  "apiVersion": "v8.8.4",
   "title": "Fusio",
   "description": "Self-Hosted API Management for Builders.",
   "paymentCurrency": "EUR",
